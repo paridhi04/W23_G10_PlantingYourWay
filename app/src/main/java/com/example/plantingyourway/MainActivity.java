a1ea32391f63c29f58bad7218deb9ab2e15dfa5d
Paridhi Gupta <42670315+paridhi04@users.noreply.github.com>
2023-03-26 15:30:38 -0700

    Initial commit

---
+++ app/src/main/java/com/example/plantingyourway/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.plantingyourway;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setTitle("Planting Your Way");
+    }
+}
