d45e388d395c94cbce3125407369e9a62fb96a2f
Paridhi Gupta <42670315+paridhi04@users.noreply.github.com>
2023-04-05 19:35:38 -0700

    Added launcher icons and SharedPref.java file.

---
+++ app/src/main/java/com/example/plantingyourway/Constants.java
@@ -0,0 +1,15 @@
+package com.example.plantingyourway;
+
+public class Constants {
+
+    public static final String REMEMBER_ME = "remember_me";
+    public static final String IS_USER = "is_user";
+    public static final String USER_DETAILS = "user_details";
+    public static final String SELECTED_SERVICES_LIST = "selected_services_list";
+    public static final String SERVICE_APPOINTMENT_DETAIL = "service_appointment_detail";
+    public static final String NEAR_PROVIDER_DETAIL = "near_provider_detail";
+    public static final String SERVICE_STATUS_BOOKED = "BOOKED";
+    public static final String SERVICE_STATUS_CLOSED = "CLOSED";
+    public static final String SERVICE_STATUS_INPROGRESS = "INPROGRESS";
+    public static final String IS_UPCOMING = "IS_UPCOMING";
+}
